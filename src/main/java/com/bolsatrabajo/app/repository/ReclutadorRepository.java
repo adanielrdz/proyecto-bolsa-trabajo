24151974981b8447e0ba25f569a92e2f1a8ce8e5
soysalvadoresai <salvadoresai00@gmail.com>
2022-11-23 22:47:23 -0600

    sobres

---
+++ src/main/java/com/bolsatrabajo/app/repository/ReclutadorRepository.java
@@ -0,0 +1,9 @@
+package com.bolsatrabajo.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bolsatrabajo.app.entity.Reclutador;
+
+public interface ReclutadorRepository extends JpaRepository<Reclutador, Long>{
+
+}
